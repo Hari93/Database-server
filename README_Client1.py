087c4be8c99091875da07aed6e2dbab71ff8b3f5
Hari93 <hama5569@colorado.edu>
2017-06-04 21:40:19 -0600

    Create README_Client1.py

---
+++ README_Client1.py
@@ -0,0 +1,4 @@
+**Client1**
+It establishes connection to the server using socket programming and it prints the time when the client is connected to server.
+# It uses json library and i am defining a dictionary data structure and i am passing the information to the server
+Also i am receiving the information from the server.
